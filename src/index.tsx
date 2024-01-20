cc2783b58b422ff20319762f9a0c27531cd4db59
zetsuboii <hmzkrb@gmail.com>
2024-01-20 17:14:09 +0300

    [feat] export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import {
   Passkey,
+  PasskeyAuthenticationRequest,
+  PasskeyRegistrationRequest,
+  CommonOptions,
+  PasskeyOptions,
   PasskeyRegistrationResult,
   PasskeyAuthenticationResult,
+  CreateOptions,
+  SignOptions,
 } from './Passkey';
 import type { PasskeyError } from './PasskeyError';
 
 export {
   Passkey,
   PasskeyError,
+  PasskeyAuthenticationRequest,
+  PasskeyRegistrationRequest,
   PasskeyRegistrationResult,
   PasskeyAuthenticationResult,
+  CommonOptions,
+  PasskeyOptions,
+  CreateOptions,
+  SignOptions,
 };
